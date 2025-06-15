68f8f14f966f95715f11ef21a2bc64b68d5a110d
Akash Sharma <aakashsharma19090@gmail.com>
2025-06-16 01:45:15 +0530

    add images in public and do cleanup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,14 @@ const compat = new FlatCompat({
     baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier')];
+const eslintConfig = [
+    ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+    {
+        rules: {
+            "no-unused-vars": "warn",
+            "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+        },
+    },
+];
 
 export default eslintConfig;
