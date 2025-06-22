c3846a237086681621860fff610643e334c1eabf
Akash Sharma <aakashsharma19090@gmail.com>
2025-06-22 14:56:22 +0530

    update actions to handle file upload

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -4,31 +4,32 @@
 
 import ImageKit from 'imagekit';
 
-const imageKit = new ImageKit({
-    publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
-    privateKey: process.env.PRIVATE_KEY!,
-    urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
-});
-
 export const ShareNewPost = async (formData: FormData) => {
     const file = formData.get('file') as File | null;
     // const description = formData.get('description') as string;
     if (file) {
-        // const arrayBuffer = await file.arrayBuffer();
-        // const buffer = Buffer.from(arrayBuffer);
-        // imageKit.upload(
-        //     {
-        //         file: buffer,
-        //         fileName: file.name,
-        //         folder: '/x-clone/posts',
-        //         transformation: {
-        //             pre: 'w-600',
-        //         },
-        //     },
-        //     function (error, result) {
-        //         if (error) console.log(error);
-        //         else console.log('----Uploaded Successfully----');
-        //     },
-        // );
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const imageKit = new ImageKit({
+            publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+            privateKey: process.env.PRIVATE_KEY!,
+            urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
+        });
+
+        imageKit.upload(
+            {
+                file: buffer,
+                fileName: file.name,
+                folder: '/x-clone/posts',
+                transformation: {
+                    pre: 'w-600',
+                },
+            },
+            function (error, result) {
+                if (error) console.log(error);
+                else console.log('----Media Uploaded Successfully----');
+            },
+        );
     }
 };
